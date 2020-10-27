31215097583fbf354c49e3e80058f0febf928702
andrewstich <andrew.stich@hotmail.com>
2020-10-27 13:17:06 -0700

    Added login page

--- store-FE/src/app/components/login-page/login-page.component.ts
+++ store-FE/src/app/components/login-page/login-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-login-page',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent implements OnInit {
+  uname: string;
+  pass: string;
+  user: User;
+  public loginForm: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = new FormGroup({
+      uname: new FormControl(''),
+      pass: new FormControl('')
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  public onSubmit(): void {
+    console.log(this.loginForm.value);
   }
 
 }
